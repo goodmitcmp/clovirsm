0369c15f159d15b651c8864727e06482b0508dac
goodmitcmp <87792043+goodmitcmp@users.noreply.github.com>
2021-07-22 17:54:53 +0900

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,1 +1,1 @@
-Invoke-WebRequest -Uri https://raw.githubusercontent.com/goodmitcmp/clovirsm/main/user-profile.ps1 -OutFile  C:/Users/user-profile1.ps1
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/goodmitcmp/clovirsm/main/user-profile.ps1 -OutFile  "C:/Users/user-profile1.ps1"
